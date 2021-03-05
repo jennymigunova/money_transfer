c5448c32711171b1dfaa14efadd0c32908038e71
Evgeniya Migunova <8880305@gmail.com>
2021-03-04 21:01:58 -0500

    Code and .gitignore added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int add = 500_000_000;
+        int sum = balance + add;
+        System.out.println(sum);
+    }
+}
